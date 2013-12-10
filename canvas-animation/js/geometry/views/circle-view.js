3e00abd99e7b5895545abe047c1d15bd5100db33
Raymond Zhou <somecrazyidiot@gmail.com>
2013-12-10 17:38:45 -0500

    canvas-animation: Add CircleView/PathView/PathEditView boilerplate definitions. Add more to Object2D template.

---
+++ canvas-animation/js/geometry/views/circle-view.js
@@ -0,0 +1,11 @@
+/*globals define*/
+define([
+  'geometry/views/object2d-view'
+], function( Object2DView ) {
+  'use strict';
+
+  var CircleView = Object2DView.extend({
+  });
+
+  return CircleView;
+});
---
+++ canvas-animation/js/geometry/views/path-edit-view.js
@@ -0,0 +1,14 @@
+/* globals define*/
+define([
+  'backbone'
+], function( Backbone ) {
+  'use strict';
+
+  /**
+   * PathEditView adds DOM elements used as handlers to modify the
+   * control points of the path.
+   */
+  var PathEditView = Backbone.View.extend({});
+
+  return PathEditView;
+});
