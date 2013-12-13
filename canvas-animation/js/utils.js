62e41e0bd2cbdc42f9f567b2a8e6adfbd2f4ff96
Raymond Zhou <somecrazyidiot@gmail.com>
2013-12-13 02:50:29 -0500

    canvas-animation: Add distance/circleContains functions to Utils.

--- canvas-animation/js/utils.js
+++ canvas-animation/js/utils.js
@@ -11,12 +11,32 @@ define(function() {
     return Math.min( Math.max( value, min ), max );
   }
 
+  function distanceSquared( x0, y0, x1, y1 ) {
+    var dx = x1 - x0,
+        dy = y1 - y0;
+
+    return dx * dx + dy * dy;
+  }
+
+  function distance( x0, y0, x1, y1 ) {
+    return Math.sqrt( distanceSquared( x0, y0, x1 ,y1 ) );
+  }
+
+  function circleContains( x, y, cx, cy, radius ) {
+    return distanceSquared( x, y, cx, cy ) < radius * radius;
+  }
+
   return {
     PI2: PI2,
 
     DEG_TO_RAD: DEG_TO_RAD,
     RAD_TO_DEG: RAD_TO_DEG,
 
-    clamp: clamp
+    clamp: clamp,
+
+    distanceSquared: distanceSquared,
+    distance: distance,
+
+    circleContains: circleContains
   };
 });
