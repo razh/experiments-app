8b63fed4d791b0b5f4498667dfcb6f3638f18643
Raymond Zhou <somecrazyidiot@gmail.com>
2013-12-13 20:10:31 -0500

    canvas-animation: Add top/bottom/left/right properties to Rect.

--- canvas-animation/js/geometry/models/rect.js
+++ canvas-animation/js/geometry/models/rect.js
@@ -22,5 +22,62 @@ define([
     }
   });
 
+  // left, right, top, and bottom properties are in world space.
+  Object.defineProperty( Rect.prototype, 'left', {
+    get: function() {
+      var halfWidth = 0.5 * this.get( 'width' );
+      return this.toWorld( -halfWidth, 0 );
+    },
+
+    set: function( position ) {
+      var halfWidth = 0.5 * this.get( 'width' );
+      var point = this.toLocal( position.x, position.y );
+      point = this.toWorld( point.x + halfWidth, 0 );
+      this.set( point );
+    }
+  });
+
+  Object.defineProperty( Rect.prototype, 'right', {
+    get: function() {
+      var halfWidth = 0.5 * this.get( 'width' );
+      return this.toWorld( halfWidth, 0 );
+    },
+
+    set: function( position ) {
+      var halfWidth = 0.5 * this.get( 'width' );
+      var point = this.toLocal( position.x, position.y );
+      point = this.toWorld( point.x - halfWidth, 0 );
+      this.set( point );
+    }
+  });
+
+  Object.defineProperty( Rect.prototype, 'top', {
+    get: function() {
+      var halfHeight = 0.5 * this.get( 'height' );
+      return this.toWorld( 0, -halfHeight );
+    },
+
+    set: function( position ) {
+      var halfHeight = 0.5 * this.get( 'height' );
+      var point = this.toLocal( position.x, position.y );
+      point = this.toWorld( 0, point.y + halfHeight );
+      this.set( point );
+    }
+  });
+
+  Object.defineProperty( Rect.prototype, 'bottom', {
+    get: function() {
+      var halfHeight = 0.5 * this.get( 'height' );
+      return this.toWorld( 0, halfHeight );
+    },
+
+    set: function( position ) {
+      var halfHeight = 0.5 * this.get( 'height' );
+      var point = this.toLocal( position.x, position.y );
+      point = this.toWorld( 0, point.y - halfHeight );
+      this.set( point );
+    }
+  });
+
   return Rect;
 });
